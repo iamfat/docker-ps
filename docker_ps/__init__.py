bd0e0117b8bef72f848f8e9a20145b46261e09ef
Jia Huang <iamfat@gmail.com>
2016-04-05 22:40:21 +0800

    first commit

---
+++ docker_ps/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import sys
+import pipes
+import getopt
+
+from tabulate import tabulate
+from docker import Client
+
+__version__ = '0.1.0'
+
+def main():
+
+    cli = Client()
+
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "v")
+    except getopt.GetoptError as _:
+        print("Usage: docker-ps [-v]")
+        sys.exit(2)
+
+    for opt, _ in opts:
+        if opt == '-v':
+            print(__version__)
+            sys.exit()
+
+    def _port_str(c):
+        if c['PublicPort'] == c['PrivatePort']:
+            return '{ip}:{port}'.format(ip=c['IP'], port=c['PublicPort'])
+        else:
+            return '{ip}:{port} <- {pport}'.format(ip=c['IP'], 
+                port=c['PublicPort'], pport=c['PrivatePort'])
+
+    t = []
+    for container in cli.containers():
+        t.append([
+            ', '.join(map(lambda c: c[1:], container['Names'])),
+            container['Image'],
+            ', '.join(map(_port_str, container['Ports'])),
+            container['Status']
+        ])
+    print(tabulate(t, headers=['CONTAINER', 'IMAGE', 'PORTS', 'STATUS']))
+
+if __name__ == "__main__":
+    main()
+
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import codecs
+import os
+import re
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding='utf-8') as fobj:
+        return fobj.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+install_requires = [
+    'requests >= 2.6.1, < 2.8',
+    'docker-py'
+]
+
+setup(
+    name='docker-ps',
+    version=find_version("docker_ps", "__init__.py"),
+    description='Simplify docker-ps output',
+    url='https://github.com/iamfat/docker-ps',
+    author="Jia Huang",
+    author_email="iamfat@gmail.com",
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='docker parse run options',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'docker-ps=docker_ps:main',
+        ],
+    },
+)
